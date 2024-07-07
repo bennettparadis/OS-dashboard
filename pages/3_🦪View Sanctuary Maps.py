cf8a58235e83e0da292ede2824ef0ff89e64a385
Bennett Paradis <60286771+bennettparadis@users.noreply.github.com>
2024-07-06 20:36:54 -0400

    Update 3_🦪View Sanctuary Maps.py

--- pages/3_🦪View Sanctuary Maps.py
+++ pages/3_🦪View Sanctuary Maps.py
@@ -42,6 +42,21 @@ st.info(
 )
 
 # ----- SIDE BAR -----
+# Define the custom CSS
+custom_css = """
+<style>
+    /* Change the color and font of the page titles in the sidebar */
+    .eczjsme13 {
+        color: #00647B !important; /* Replace with your desired color */
+        font-weight: bold !important;
+        font-size: 18px !important;
+    }
+</style>
+"""
+
+# Apply the custom CSS
+st.markdown(custom_css, unsafe_allow_html=True)
+
 sanctuary_names = sorted(df["OS_Name"].unique())
 default_sanctuary1 = "Neuse River"
 
