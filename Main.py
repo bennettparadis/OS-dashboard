54cf0e687df855d66edac90d48c58b64a875c2bb
Bennett Paradis <60286771+bennettparadis@users.noreply.github.com>
2024-07-25 15:41:31 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -32,29 +32,7 @@ df_selection = df.query(
     "Year == @year"
 )
 
-sampling_effort = {
-    '2019': {
-        'samples':'109',
-        'oysters': '4,995'
-    },
-    '2020': {
-        'samples':'123',
-        'oysters': '17,898'
-    },
-    '2021': {
-        'samples':'128',
-        'oysters':'16,637'
-    },
-    '2022': {
-        'samples':'146',
-        'oysters':'18,255'
-    },
-    '2023': {
-        'samples':'136',
-        'oysters':'26,652'
-    },
-}
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([0.8,1])
 
 with col1:
     text.display_text("On this app you have the ability to interact with the Oyster Sanctuary dataset!", font_size=18, align='left')
@@ -73,6 +51,7 @@ with col1:
     map_views = st.page_link("pages/3_🦪View Sanctuary Maps.py", label="View the blueprints for each sanctuary", icon="🦪")
     compare = st.page_link("pages/4_📊Compare Population Data.py", label="Compare oyster populations over time", icon="📊")
     analyze = st.page_link("pages/5_🤿Analyze Reef Materials.py", label="Analyze the efficacy of different reef materials", icon="🤿")
+
 
 with col2:
     # Donut chart
@@ -106,5 +85,5 @@ with col2:
     )
 
      # Displaying the figure in Streamlit
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
     text.display_text("This donut chart illustrates how many legal, sublegal, and spat oysters were sampled during the annual monitoring of North Carolina's oyster sanctuaries. Select a year in the side bar to change the data & the graph above.")
